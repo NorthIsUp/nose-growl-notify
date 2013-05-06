40066bb37eab899af08c4be2889a62821fb3cba5
Adam Hitchcock <adam@northisup.com>
2013-05-06 14:14:01 -0700

    rename

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ with open('README.rst') as f:
     long_description = f.read()
 
 setup(
-    name='NoseGrowlNotify',
-    author='Fernando Macedo',
-    author_email='fgmacedo@gmail.com',
-    description='Nose plugin for Growl notifications',
+    name='nose-growl-notify',
+    author='Adam Hitchcock',
+    author_email='adam@northisup.com',
+    description='Nose plugin for Growl notifications with sublime url',
     long_description=long_description,
     license='Creative Commons Attribution license (CC BY)',
     install_requires=['nose>=0.10', 'gntp'],
-    url="http://github.org/fgmacedo/nose-growl-notify",
+    url="http://github.org/northisup/nose-growl-notify",
     version='0.1',
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     package_data={
-        'NoseGrowlNotify': ['nosegrowlnotify/*.png'],
+        'NoseGrowlNotify': ['nose-growl-notify/*.png'],
     },
     entry_points={
         'nose.plugins.0.10': [
